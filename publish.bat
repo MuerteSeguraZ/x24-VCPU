dada5ab1f1f73dcdb91255dfa11326a5e30f9ca1
MuerteSeguraZ <anneud.pedemonte.t@gmail.com>
2025-08-26 21:13:10 +0200

    increment/decrement shortcuts and negation

--- publish.bat
+++ publish.bat
@@ -1,19 +1,14 @@
 @echo off
-REM Switch to main branch (create if it doesn't exist)
-git checkout main 2>nul
-if %errorlevel% neq 0 git checkout -b main
 
-REM Add all changes
 git add --all
 
-REM Commit with fixed message
-git commit -m "increment/decrement shortcuts and negation"
+set "MSG=%~1"
+if "%MSG%"=="" set "MSG=increment/decrement shortcuts and negation"
 
-REM Set remote origin if not already set
-git remote | findstr origin >nul
-if %errorlevel% neq 0 git remote add origin https://github.com/MuerteSeguraZ/x24.git
+git status
 
-REM Push to remote main
-git push -u origin main
+git commit -m "%MSG%"
+
+git push origin main
 
 pause
